ed8d06c24560aae3fd19327a83813ac2b521a4f3
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-01-06 21:18:45 -0800

    logic.js update on eventlistners

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,6 +4,7 @@ var sportsCar = ['Lamborghini', 'Ferrari', 'Porsche 911', 'Audi TT', 'Maserati',
 $(document).ready(function() {
 
 // create buttons for each sportscar and then render to HTML
+
 function renderButtons() {
 
     $('#carButton').empty();
@@ -23,7 +24,10 @@ function renderButtons() {
       $('#carButton').append(a);
     }
   }
+    renderButtons();
+  
   // This function handles events when a car button is clicked
+ 
   $('#add-car').on('click', function() {
     // This line grabs the input from the textbox
     var car = $('#car-input').val().trim();
@@ -31,14 +35,45 @@ function renderButtons() {
     sportsCar.push(car);
     renderButtons();
     return false;
+  });
+
+  //displays the giphy images
+
+  $(document).on('click', function() {
   
-})
+  $("#carImages").empty();
+  
+  var a = $(this).attr('data-name');
+  // var apiKey = "L7AcBNBpJoXhk4n3G6tfG051LWUOMI9x";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a + "&api_key=dc6zaTOxFJmzC&limit=12";
+    
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+      })
+    
+      .done(function(response) {
+          console.log(response);
+        
+        var results = response.data;
 
-//   // Adding a click event listener to all elements with a class of "movie"
-//     $(document).on("click", ".movie", displayMovieInfo);
+        for (var i = 0; i < results.length; i++){
+          var carDiv = $('<div class="item">');
+          var rating = results[i].rating;
+          var rate = $('<p>').text("Rating: " + rating)
+          var gifImage = $('<img>');
+          gifImage.attr('src', results[i].images.fixed_height_still.url)
+		            .attr('data-still', results[i].images.fixed_height_still.url)
+		            .attr('data-animate', results[i].images.fixed_height.url)
+		            .attr('data-state', "still")
+		            .addClass("showImage");
+         $('#carImages').prepend(carDiv);
+        
+        
+        }
+    });
 
-  // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+});
 
 
 });
