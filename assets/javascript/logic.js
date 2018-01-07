ed224438a53c71e1d7b4801241dfdae6fdb0e2b1
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-01-07 15:57:11 -0800

    enternd in code fordiplaying Giphy images

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,7 @@
 var sportsCar = ['Lamborghini', 'Ferrari', 'Porsche 911', 'Audi TT', 'Maserati', 'BMW M3', 'Ford Mustang', 'Chevy Camero', 'Nissan GTR', 'Lexus RC'];
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
 // create buttons for each sportscar and then render to HTML
 
@@ -29,6 +29,9 @@ function renderButtons() {
   // This function handles events when a car button is clicked
  
   $('#add-car').on('click', function() {
+
+    event.preventDefault();
+
     // This line grabs the input from the textbox
     var car = $('#car-input').val().trim();
      // Adding car from the textbox to our array
@@ -39,41 +42,44 @@ function renderButtons() {
 
   //displays the giphy images
 
-  $(document).on('click', function() {
-  
-  $("#carImages").empty();
-  
-  var a = $(this).attr('data-name');
-  // var apiKey = "L7AcBNBpJoXhk4n3G6tfG051LWUOMI9x";
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a + "&api_key=dc6zaTOxFJmzC&limit=12";
+  $('#carButton').on('click', function() {
+    
+    var cars = $(this).attr('data-cars'); 
+    var apiKey = "L7AcBNBpJoXhk4n3G6tfG051LWUOMI9x";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cars + "&api_key=" + apiKey + "&limit=10";
+    console.log(queryURL);
     
     $.ajax({
-        url: queryURL,
-        method: 'GET'
-      })
-    
-      .done(function(response) {
-          console.log(response);
-        
-        var results = response.data;
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+      $('#carImages').empty();
 
-        for (var i = 0; i < results.length; i++){
-          var carDiv = $('<div class="item">');
-          var rating = results[i].rating;
-          var rate = $('<p>').text("Rating: " + rating)
-          var gifImage = $('<img>');
-          gifImage.attr('src', results[i].images.fixed_height_still.url)
-		            .attr('data-still', results[i].images.fixed_height_still.url)
-		            .attr('data-animate', results[i].images.fixed_height.url)
-		            .attr('data-state', "still")
-		            .addClass("showImage");
-         $('#carImages').prepend(carDiv);
-        
-        
-        }
-    });
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+      // console.log(results);
+      
+      var gifDiv = $("<div class='item'>");
 
-});
+      // Storing the result item's rating
+      var rating = results[i].rating;
 
+      // Creating a paragraph tag with the result item's rating
+      var p = $("<p>").text("Rating: " + rating);
 
+      // Creating an image tag
+      var carImage = $("<img>");
+
+      // Giving the image tag an src attribute of a proprty pulled off the
+      carImage.attr("src", results[i].images.fixed_height.url);
+
+      // Appending the paragraph and carImage created to the "gifDiv" div created
+      gifDiv.append(p);
+      gifDiv.append(carImage);
+
+      // Prepending the gifDiv to the "#carImages" div in the HTML
+      $("#carImages").prepend(gifDiv);
+    }
+  })
 });
